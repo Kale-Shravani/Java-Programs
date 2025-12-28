19f4908e63e7e1fdae47268e3c9dada1b4b2a19b
Kale-Shravani <shravanikale38@gmail.com>
2025-12-28 13:57:11 +0530

    Linear Search code on 2D Array

---
+++ linearsearch.java
@@ -0,0 +1,37 @@
+//searching in 2D array
+
+public class linearsearch {
+    public static void main(String[] args) {
+        int[][] arr={
+          {1,2,3},
+          {4,5,6,7},
+          {8,9,10,11,12},
+          {13,14,15,16,17}
+        };
+        
+        int target=10;
+        search(arr, target);
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+    static void search(int arr[][],int target) {
+        boolean found=false;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                if(arr[i][j]==target){
+                    System.out.println("Target found at index" + i + " " + j );
+                    found=true;
+                    break;
+                }
+            }
+        }
+        if(!found){
+            System.out.println("not found..!");
+        }
+    }
+}
